535cabd59a732e1d4764307647640dd2033d0073
rafaelleeto <leetobr@gmail.com>
2025-04-16 21:54:42 -0300

    Implementação do sistema de menu, criação de pedidos, novo banco de dados (tipos), opção de cadastro, login, lista de todos os tipos de pizzas usando CSS.

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.secret_key = "senha_muito_segura"
 def home():
     return render_template("home.html")
 
-@app.route("/cadastro",methods = ["GET", "POST"])
+@app.route("/cadastro", methods = ["GET", "POST"])
 def cadastro():
     if request.method == "GET":
         return render_template("cadastro.html")
@@ -18,15 +18,51 @@ def cadastro():
     database.criar_conta(email, senha)
     return redirect("/login")
     
-    
-    
-    
-
-
-@app.route("/login",methods = ["GET", "POST"])
+@app.route("/login", methods = ["GET", "POST"])
 def login():
     if request.method == "GET":
         return render_template("login.html")
+    email = request.form["email"]
+    senha = request.form["senha"]
+    senha_banco = database.localizar_usuario(email)
+
+    if database.localizar_usuario(email):
+        senha_banco = database.localizar_usuario(email)
+        if senha == senha_banco[0]:
+            session['email'] = email
+            return redirect("/menu")
+    return "Parece que algo deu errado, tente novamente!"
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+        
+@app.route("/criar_pizza", methods = ["GET", "POST"])
+def criar_pizza():
+    if request.method == "GET":
+        return render_template("criar_pizza.html")
+    nome = request.form['nome']
+    ingredientes = request.form['ingredientes']
+    database.criar_pizza(nome,ingredientes)
+    return redirect("/menu")
+
+@app.route ("/editar_pizza")
+def editar_pizza():
+    pizzas=database.pegar_tipos_de_pizzas()
+    return render_template("editar_pizza.html", pizzas = pizzas)
+
+@app.route("/atualizar/<nome>", methods = ["GET", "POST"])
+def atualizar_nome_da_pizza(nome):
+    if request.method == "GET":
+        pizza = database.pegar_uma_pizza(nome)
+        return render_template("atualizar_pizza.html", pizza = pizza)
+    nome_novo = request.form['nome']
+    ingredientes = request.form['ingredientes']
+    database.atualizar_pizza(nome_novo,ingredientes,nome)
+    return redirect("/menu")
+
+    
+
     
 
 
--- database.py
+++ database.py
@@ -25,6 +25,12 @@ def criar_tabelas():
                    nome_cliente TEXT,
                    status TEXT
                    )   ''')
+    
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS tipos (
+                   nome TEXT primary key,
+                   ingredientes TEXT
+                   )   ''')
 
 def criar_conta(email, senha):
     conexao = conectar_banco()
@@ -33,7 +39,51 @@ def criar_conta(email, senha):
     cursor.execute("""INSERT INTO usuarios VALUES (?,?)""",(email, senha))
     conexao.commit()
     cursor.close()
-    
+
+def localizar_usuario(email):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute("""SELECT senha FROM usuarios WHERE email=?""",(email,))
+    return cursor.fetchone()
+
+def criar_pizza(nome, ingredientes):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute("""INSERT INTO tipos VALUES (?,?)""",(nome, ingredientes))
+    conexao.commit()
+    cursor.close()
+
+def pegar_tipos_de_pizzas():
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute("""SELECT * FROM tipos""")
+    return cursor.fetchall()
+
+def pegar_uma_pizza(nome):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute("""SELECT * FROM tipos where nome=?""",(nome,))
+    return cursor.fetchone()
+
+def atualizar_pizza(nome_novo, ingredientes, nome):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute("""UPDATE tipos set nome=?, ingredientes=? where nome=?""",(nome_novo, ingredientes, nome))
+    conexao.commit()
+    cursor.close()
+
+def deletar_pizza(nome):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute("""DELETE FROM tipos WHERE nome=? """,(nome,))
+    conexao.commit()
+    cursor.close()
+
+
+
+
+
+
     
 if __name__=="__main__":
     criar_tabelas()
